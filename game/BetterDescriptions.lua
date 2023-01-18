7ebec53d00887944f542082f92c949006348f718
berylliumquestion <49571759+berylliumquestion@users.noreply.github.com>
2023-01-18 14:39:19 -0500

    Changed all hard tabs to 4 spaces

--- game/BetterDescriptions.lua
+++ game/BetterDescriptions.lua
@@ -1,22 +1,22 @@
 function fixPrisonModeDescriptions()
-	Prison["modes"] = {
-		{
-			id = "ConvictLabor",
-			name = "[[Prison mode1_name][Use Convict Labor][Prison name]]",
-			param1 = 3,
-			rollover = "Work, you dogs! Every prisoner will generate $3 profit per month and will be released in 3 years."
-		},
-		{
-			id = "Reeducation",
-			name = "[[Prison mode2_name][Reeducation][Prison name]]",
-			param1 = 20,
-			rollover = "A wide range of re-educational \"I love Presidente\" courses are offered. The respect of all prisoners is raised by 20% and will be released in 20 years."
-		}
-	}
+    Prison["modes"] = {
+        {
+            id = "ConvictLabor",
+            name = "[[Prison mode1_name][Use Convict Labor][Prison name]]",
+            param1 = 3,
+            rollover = "Work, you dogs! Every prisoner will generate $3 profit per month and will be released in 3 years."
+        },
+        {
+            id = "Reeducation",
+            name = "[[Prison mode2_name][Reeducation][Prison name]]",
+            param1 = 20,
+            rollover = "A wide range of re-educational \"I love Presidente\" courses are offered. The respect of all prisoners is raised by 20% and will be released in 20 years."
+        }
+    }
 end
 
 function fixArrestDescription()
-	EdictArrest["rollover"] = "A policeman will arrest and detain the target for 3 or 20 years if sent to regular prison, or a million years if sent to labor camp. Sentence only begins as soon as arestee enters the prison. This will decrease the respect of the target's family. May or may not decrease respect of target depending on building mode chosen if sent to regular prison."
+    EdictArrest["rollover"] = "A policeman will arrest and detain the target for 3 or 20 years if sent to regular prison, or a million years if sent to labor camp. Sentence only begins as soon as arestee enters the prison. This will decrease the respect of the target's family. May or may not decrease respect of target depending on building mode chosen if sent to regular prison."
 end
 
 local FiredOnce = false
@@ -26,7 +26,7 @@ OnMsg.UASetMode = function(actions,mode)
             FiredOnce = true
             CreateRealTimeThread(function()
                 fixPrisonModeDescriptions()
-				fixArrestDescription()
+                fixArrestDescription()
             end)
         end
     end

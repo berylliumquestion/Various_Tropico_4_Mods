8ab0049cf06f9e273465d22a5d310915034a77e3
berylliumquestion <49571759+berylliumquestion@users.noreply.github.com>
2023-01-19 15:57:41 -0500

    Added a batch file installer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 January 18, 2023
 BetterDescriptions.lua - VER: 0.0.0
     - Fixes the description of how long prisoners are held for.
+
+January 19, 2023
+Installer.bat - VER: 0.0.0
+    - New automated process for installing mods. Will be expanded in the future.
---
+++ Installer.bat
@@ -0,0 +1,34 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+set mods[0]=BetterDescriptions
+set choice=
+set choices=
+
+for  /L %%i in (0, 1, 0) do (
+	set /p choice="Install !mods[%%i]!? (y/n): "
+	set choices[%%i]=!choice!
+)
+
+if exist "C:\GOG Games\Tropico 4\" (
+	if not exist game\ (
+		mkdir "C:\GOG Games\Tropico 4\game"
+	)
+	
+	for /L %%i in (0, 1, 0) do (
+		if !choices[%%i]!==y (
+			if !mods[%%i]!==BetterDescriptions (
+				copy /v "game\BetterDescriptions.lua" "C:\GOG Games\Tropico 4\game\BetterDescriptions.lua"
+			)
+			
+		) else (
+			echo skipped
+		)
+	)
+	
+) else (
+	echo Unimplemented
+)
+
+endlocal

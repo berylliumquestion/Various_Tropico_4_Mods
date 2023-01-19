5b67a237f13a034b2338d2a50f49b7ffb8a266ca
berylliumquestion <49571759+berylliumquestion@users.noreply.github.com>
2023-01-19 17:21:41 -0500

    Adding automatic fetching of hpk tool

---
+++ Extract.ps1
@@ -0,0 +1,1 @@
+Expand-Archive -Path hpk.zip .
--- Installer.bat
+++ Installer.bat
@@ -2,6 +2,15 @@
 
 setlocal EnableDelayedExpansion
 
+if not exist hpk.zip (
+    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%cd%\Download.ps1'"
+)
+
+if not exist hpk/ (
+    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%cd%\Extract.ps1'"
+    rename hpk-v0.3.11-x86_64-pc-windows-msvc hpk
+)
+
 set mods[0]=BetterDescriptions
 
 for  /L %%i in (0, 1, 0) do (

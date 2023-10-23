7ae74a3a0a5639c1f8742dbef15ad412c1dfcb08
berylliumquestion <49571759+berylliumquestion@users.noreply.github.com>
2023-10-23 16:07:57 -0400

    Remove deprecated code
    Since the sequence lists are now being directly edited by the
    NoFreeImmigrantsMeansNoFreeImmigrants mod, it is now not necessary
    to attempt to clobber all of the minister events.

--- Download.ps1
+++
@@ -1,3 +0,0 @@
-Import-Module BitsTransfer
-
-Start-BitsTransfer -Source https://github.com/nickelc/hpk/releases/download/v0.3.11/hpk-v0.3.11-x86_64-pc-windows-msvc.zip -Destination hpk.zip
--- Installer.bat
+++ Installer.bat
@@ -2,15 +2,6 @@
 
 setlocal EnableDelayedExpansion
 
-if not exist hpk.zip (
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%cd%\Download.ps1'"
-)
-
-if not exist hpk/ (
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%cd%\Extract.ps1'"
-    rename hpk-v0.3.11-x86_64-pc-windows-msvc hpk
-)
-
 echo Default or non-default location?
 echo 1)Default GOG install
 echo 2)Default Steam install
@@ -44,6 +35,10 @@ if not exist "!directory!\game" (
     mkdir "!directory!\game"
 )
 
+if not exist "!directory!\NuclearTakedown\game" (
+    mkdir "!directory!\NuclearTakedown\game"
+)
+
 for /L %%i in (0, 1, 1) do (
     if !choices[%%i]!==y (
         if !mods[%%i]!==BetterDescriptions (
@@ -51,68 +46,10 @@ for /L %%i in (0, 1, 1) do (
         )
             
         if !mods[%%i]!==NoFreeImmigrants (
-            call :InstallNoFreeImmigrants "!directory!"
+            copy /v "mods\NoFreeImmigrantsMeansNoFreeImmigrants.lua" "!directory!\NuclearTakedown\game\NoFreeImmigrantsMeansNoFreeImmigrants.lua"
         )
             
     ) else (
         echo skipped
     )
 )
-
-endlocal
-goto :EOF
-
-:InstallNoFreeImmigrants
-
-set dlc_hpks[0]=Construction
-set dlc_hpks[1]=EastPoint
-set dlc_hpks[2]=Expansion
-set dlc_hpks[3]=Megalopolis
-set dlc_hpks[4]=Military
-set dlc_hpks[5]=NuclearTakedown
-set dlc_hpks[6]=PirateHaven
-set dlc_hpks[7]=Plantation
-set dlc_hpks[8]=Propaganda
-set dlc_hpks[9]=Update1
-set dlc_hpks[10]=Vigilante
-set dlc_hpks[11]=Voodoo
-
-if not exist "%~1\dlc.bak\" (
-    echo Backing up hpks!
-    echo %~1
-    robocopy "%~1\dlc" "%~1\dlc.bak"
-    copy "%~1\Packs\boot\persist\Game.hpk" "%~1\Packs\boot\persist\Game.hpk.bak"
-)
-
-for /L %%i in (0, 1, 11) do (
-    echo Extracting !dlc_hpks[%%i]!.hpk...
-    hpk\hpk.exe extract "%~1\dlc\!dlc_hpks[%%i]!.hpk" "%~1\dlc\!dlc_hpks[%%i]!"
-)
-
-hpk\hpk.exe extract "%~1\Packs\boot\persist\Game.hpk" "%~1\Packs\boot\persist\Game"
-
-for /L %%i in (0, 1, 11) do (
-    echo Deleting minister events from !dlc_hpks[%%i]!.hpk...
-    if not !dlc_hpks[%%i]!==Expansion (
-        del "%~1\dlc\!dlc_hpks[%%i]!\!dlc_hpks[%%i]!\game\SequenceList\Minister_events.lua
-    ) else (
-        del "%~1\dlc\!dlc_hpks[%%i]!\ex\game\SequenceList\Minister_events.lua
-    )
-)
-
-del "%~1\Packs\boot\persist\Game\game\SequenceList\Minister_events.lua"
-
-for /L %%i in (0, 1, 11) do (
-    echo Repacking !dlc_hpks[%%i]!.hpk...
-    hpk\hpk.exe create "%~1\dlc\!dlc_hpks[%%i]!" "%~1\dlc\!dlc_hpks[%%i]!.hpk" --dont-compress-files
-)
-
-hpk\hpk.exe create "%~1\Packs\boot\persist\Game" "%~1\Packs\boot\persist\Game.hpk" --dont-compress-files
-
-if exist "%~1\dlc\NuclearTakedown.hpk" (
-    copy mods\NoFreeImmigrantsMeansNoFreeImmigrants.lua "%~1\NuclearTakedown\game\NoFreeImmigrantsMeansNoFreeImmigrants.lua"
-)
-
-exit /B 0
-
-:EOF
